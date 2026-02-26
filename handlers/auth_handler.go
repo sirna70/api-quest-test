7e36ee099e9a29dd2e0068eef0415e1ba7b95505
rikki70 <v.rikki.nn@banksinarmas.com>
2026-02-27 02:27:43 +0700

    fixing: update10

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -11,12 +11,32 @@ import (
 var JwtSecret = []byte("secret123")
 
 func GenerateToken(c *gin.Context) {
+	var creds struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		return
+	}
+
+	// VALIDATION
+	if creds.Username != "admin" || creds.Password != "password" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+		return
+	}
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user": "admin",
+		"user": creds.Username,
 		"exp":  time.Now().Add(time.Hour * 1).Unix(),
 	})
 
-	tokenString, _ := token.SignedString(JwtSecret)
+	tokenString, err := token.SignedString(JwtSecret)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not generate token"})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
